12211e7ce81c2289004220c59c3cd35a8c643e62
Sarah Abouelela <abouelelas@gmail.com>
2020-07-29 10:42:50 -0400

    added changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-// const axios = require("axios"); not needed 
 const inquirer = require("inquirer");
 // const generateMarkdown = require("./utils/generateMarkdown.js");
 
